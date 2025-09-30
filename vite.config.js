415b1cb5a5bb651ea782c8cae721c15289e92764
nytopop <ericizoita@gmail.com>
2025-09-30 12:25:57 -0700

    +definition in input fields, +version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { execSync } from "child_process";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+
+  define: {
+    __GIT_VERSION__: JSON.stringify(
+      execSync("git describe --always --dirty").toString("utf8").trim(),
+    ),
+  },
 });

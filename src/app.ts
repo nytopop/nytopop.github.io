c005e74855a45dff7da09791a18dda8de94d4d3e
nytopop <ericizoita@gmail.com>
2025-10-02 13:22:49 -0700

    onwards

--- src/app.ts
+++ src/app.ts
@@ -17,54 +17,85 @@ import Alpine from "alpinejs";
 import OpenAI from "openai";
 
 export class App {
-  // config (FIXME: these are w/e $persist type is...)
-  apibase: string | null | any;
-  apikey: string | null | any;
-  model: string | null | any;
-  antiglaze: boolean | any;
+  // config
+  apiurl = Alpine.$persist(null);
+  apikey = Alpine.$persist(null);
+  model = Alpine.$persist(null);
+  glazing = Alpine.$persist("suppress");
+  deception = Alpine.$persist("suppress");
+  aislop = Alpine.$persist("suppress");
+  rolemodel = Alpine.$persist(null);
 
-  // ui
-  configmodal: boolean;
+  // state
+  configmodal: boolean = false;
+  actions: Action[] = [];
 
-  constructor() {
-    // config
-    this.apibase = Alpine.$persist(null);
-    this.apikey = Alpine.$persist(null);
-    this.model = Alpine.$persist(null);
-    this.antiglaze = Alpine.$persist(true);
-
-    // ui
-    this.configmodal = false;
-  }
+  // TODO: context
+  // TODO: store usage stats on tokens, steps, etc in persistent storage
 
   autosize(bar: HTMLTextAreaElement): void {
     bar.style.height = "";
     bar.style.height = bar.scrollHeight + "px";
   }
 
-  omnifunc(omni: string): void {
+  collapse(bar: HTMLTextAreaElement): void {
+    bar.style.height = "";
+  }
+
+  apiclient(): OpenAI {
+    const baseURL = !!this.apiurl
+      ? (this.apiurl as unknown as string)
+      : "https://api.openai.com/v1/";
+
+    return new OpenAI({
+      baseURL: baseURL,
+      apiKey: this.apikey as unknown as string,
+      dangerouslyAllowBrowser: true,
+    });
+  }
+
+  async apistatus(): Promise<string> {
+    try {
+      const models = await this.apiclient().models.list();
+
+      if (
+        this.model &&
+        !models.data.some((m) => m.id === (this.model as unknown as string))
+      ) {
+        return "unknown model";
+      }
+
+      return "ok";
+    } catch (err: any) {
+      return err.status ? err.status : err.message;
+    }
+  }
+
+  // TODO: write display logic => fill
+  async omnifunc(omni: string): Promise<void> {
     // TODO: write the omnifunc???
-    // [ ] decide how to score action selections in a common reference frame
-    // [ ] determine what kind of input we got (command? natural language? etc)
-    // [ ] rather, amongst all the shit we can do make a top-K spread
+    // [x] decide how to score action selections in a common reference frame [reranking task]
+    //
+    // [ ] figure out action generators over inputs ('here are possible actions in this context')
+    //     ex: a 'config' builtin that does literal matching
+    //
+    // [ ] default task for reranking: coherent extrapolated volition (CEV) of input sender
+    //     note: we want several (and we should permit 'compound' tasks (ux: display it somehow idk))
+    //
+    // [ ] figure out interface for how either of the above are applied (interface?)
+    //
+    // roughly, we need to produce a list of at most K potential actions based on input and ctx state.
+
+    // pipeline so far: input ~> fanout ~> merge + rerank ~> top k
     console.log(omni);
   }
 
   doaction(): void {
     // TODO: lookup the action in our persist list -> do it
+    // (action: string)
   }
+}
 
-  mkclient(): OpenAI {
-    const baseURL = !!this.apibase
-      ? this.apibase
-      : "https://api.openai.com/v1/";
-
-    const client = new OpenAI({
-      baseURL: baseURL,
-      apiKey: this.apikey,
-      dangerouslyAllowBrowser: true,
-    });
-
-    return client;
-  }
+interface Action {
+  show: string; //
 }
